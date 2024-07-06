24f3ac8ee844189308c83703fc396f490ed6f9c0
day <dayblas007@gmail.com>
2024-07-06 19:21:42 -0300

    Modifique archivo README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# blas_Proyectofinal
+
+Proyecto Final Coder House - Python: UNO
+Comisión: 54150
+Profes: 
+Alumno: Dayana E Blas
+Nombre del Proyecto: UNO
+
+
+##-Descripción del proyecto
+El proyecto “UNO” consiste en una página web diseñada para que emprendedores puedan dar a conocer sus emprendimientos. Algunas características clave son:
+
+###Registro y autenticación: Los usuarios deben registrarse para acceder a la creación de publicaciones y enviar mensajes. Una vez registrados, pueden iniciar sesión y acceder a su perfil para editarlo.
+
+##-Tecnologías utilizadas
+El proyecto utiliza las siguientes tecnologías:
+
+- CSS: 84.8%
+- Python: 7.9%
+- HTML: 7.2%
+- JavaScript: 0.1%
--- mensajeria/views.py
+++ mensajeria/views.py
@@ -2,25 +2,26 @@ from django.shortcuts import render
 from mensajeria.models import Mensaje
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView , UpdateView , DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
    
-class CrearMensaje(CreateView):
+class CrearMensaje(LoginRequiredMixin,CreateView):
     model = Mensaje
     template_name = 'mensajeria/crear_mensaje.html'
     success_url = reverse_lazy('lista_mensajes')
     fields = '__all__'
 
 
-class EliminarMensaje(DeleteView):
+class EliminarMensaje(LoginRequiredMixin,DeleteView):
     model = Mensaje
     template_name = 'servicios/eliminar_mensaje.html'
     success_url = reverse_lazy('mensajes')
 
   
-class ListaMensajes(ListView):
+class ListaMensajes(LoginRequiredMixin,ListView):
     model = Mensaje
     template_name = 'mensajeria/lista_mensaje.html'
     
