b00582780d45bc24317ee667a90fc3dec27a356b
day <dayblas007@gmail.com>
2024-07-11 10:25:39 -0300

    ULTIMO

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
+
 def inicio(request):
     return render(request,'inicio/inicio.html')
 
-
+# ACERCA DE MI
 def aboutme(request):
     return render(request, "inicio/aboutme.html")
 
